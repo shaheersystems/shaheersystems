7982f66d0b77d8e9c422cb012132859f7837b3a4
shaheerinit <105648279+shaheersystems@users.noreply.github.com>
2022-10-03 15:37:56 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div className="h-screen bg-black w-full flex items-center justify-center">
+        <h1 className="text-8xl text-white font-bold text-center">
+          Coming Soon...
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
